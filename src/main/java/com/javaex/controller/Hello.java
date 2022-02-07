089a83bcd2448e56b5a0b3e2acf821f79e30319c
구민석 <ser_om@naver.com>
2022-02-08 00:17:20 +0900

    02/08

--- src/main/java/com/javaex/controller/Hello.java
+++ src/main/java/com/javaex/controller/Hello.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Hello {
-	@RequestMapping("/hello")
-	public String hello() {
+public class Hello{
+
+	
+	@RequestMapping( "/hello")
+	public String hello(){
 		System.out.println("/hellospring/hello");
 		return "/WEB-INF/views/index.jsp";
 	}
--- webapp/WEB-INF/views/index.jsp
+++ webapp/WEB-INF/views/index.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<h1>안녕하세요?</h1>
-
+	<h1>h1>hello spring 헬로 스프링</h1>
 </body>
 </html>
